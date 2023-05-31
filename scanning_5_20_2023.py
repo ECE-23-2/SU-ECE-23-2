abfb25eea6e0594d9c38fdfebd27decc11814975
ECE-23-2 <124209424+ECE-23-2@users.noreply.github.com>
2023-05-31 12:15:31 -0700

    Update scanning_5_20_2023.py

--- scanning_5_20_2023.py
+++ scanning_5_20_2023.py
@@ -8,25 +8,30 @@ from pandas import DataFrame
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import RobustScaler
 
+#Getting the shortest distance for landing gear
 def getting_shortest_distance(Distance,Angle):
     min_dist_index = np.argmin(Distance)
     min_dist = Distance[min_dist_index]
     min_angle = Angle[min_dist_index]
     return min_dist,min_angle
 
+#Converting cartesian coordinates to polar coordinates
 def car2pol(x,y):
     r = np.sqrt(x**2 + y**2)
     theta = np.arctan2(y,x)
     return r , theta
 
+#Converting polar coordinates to cartesian coordinates for x
 def pol2carteisan_x(rho,theta):
     x = rho * math.cos(theta)
     return x
 
+#Converting polar coordinates to cartesian coordinates for y
 def pol2carteisan_y(rho,theta):
     y = rho * math.sin(theta)
     return y
 
+#Ploting x and y
 def plot(x,y):
     # plot
     #plt.plot(y, x, color='green', linestyle='', linewidth=0.5,
@@ -36,6 +41,7 @@ def plot(x,y):
     plt.ylabel('Y')
     plt.show()
 
+#Removing the distances that are zero from LiDAR data
 def remove_zero_distance(distances, angles):
   new_distances = []
   new_angles = []
@@ -47,18 +53,18 @@ def remove_zero_distance(distances, angles):
 
 def simple_scan():
     lidar = PyRPlidar()
-    lidar.connect(port="COM6", baudrate=256000, timeout=3)
+    lidar.connect(port="/dev/ttyUSB0", baudrate=256000, timeout=3)
     # Linux   : "/dev/ttyUSB0"
     # MacOS   : "/dev/cu.SLAB_USBtoUART"
     # Windows : "COM5"
 
-    lidar.set_motor_pwm(800)
+    lidar.set_motor_pwm(700)
     time.sleep(5)
     arr_dist = []
     arr_angle = []
     len_count = []
 
-    #scan_generator = lidar.get_scan_modes()  # 16K
+    #Using boost mode for scanning to get optimize for sample rate
     scan_generator = lidar.start_scan_express(mode = 2) #2 or 3
     for count, scan in enumerate(scan_generator()):
         # print(count, scan.distance)
@@ -71,6 +77,7 @@ def simple_scan():
     lidar.disconnect()
     return arr_dist,arr_angle,len_count
 
+#Extracting three landing gear
 def data_processing():
     dist,angle,count =simple_scan()
     radius = 130
@@ -84,19 +91,29 @@ def data_processing():
 
     # Remove Zero distances
     distance_non_zero, angles_non_zero = remove_zero_distance(distance, arr_angle_rounded)
+    index_remove = []
+    for i in range(len(distance_non_zero)):
+        if distance_non_zero > 3000:
+            index_remove.append(i)
 
+    remove_distance = np.delete(distance_non_zero, index_remove)
+    remove_angle = np.delete(angles_non_zero, index_remove)
+    # Sorted by angle
+    Sorted_indices = np.argsort(remove_angle)
+    sorted_distance = remove_distance[Sorted_indices]
+    sorted_angle = remove_angle[Sorted_indices]
     # Convert to X and Y
     X1, Y1 = [], []
-    for i in range(len(distance_non_zero)):
-        X1.append(pol2carteisan_x(distance_non_zero[i], angles_non_zero[i]))
-        Y1.append(pol2carteisan_y(distance_non_zero[i], angles_non_zero[i]))
+    for i in range(len(sorted_distance)):
+        X1.append(pol2carteisan_x(sorted_distance[i], sorted_angle[i]))
+        Y1.append(pol2carteisan_y(sorted_distance[i], sorted_angle[i]))
 
     # Distance between data points
+    # Remove the data points that are greater than threshold or data points that are too close to each other
     index1 = []
     for i in range(len(X1)):
-        distance = 0
+        #distance = 0
         distance = np.sqrt((X1[i] - X1[i - 1]) ** 2 + (Y1[i] - Y1[i - 1]) ** 2)
-        # if distance < 3 or distance > 30: #remove
         if distance < 2 or distance > 30:  # remove
             index1.append(i)
 
@@ -114,7 +131,7 @@ def data_processing():
         BC = math.sqrt((remove_x[i + 2] - remove_x[i + 1]) ** 2 + (remove_y[i + 2] - remove_y[i + 1]) ** 2)
         arc_tan = np.arctan2(BC, AB)
         # if arc_tan < 0.6 or arc_tan> 0.9:
-        if arc_tan < 0.2 or arc_tan > 0.9:
+        if arc_tan < 0.2 or arc_tan > 2:
             index_to_remove.append(i)
 
     X_Vector = np.delete(remove_x, index_to_remove)
@@ -124,20 +141,20 @@ def data_processing():
     # GROUP
     # DBSCAN
     # Cluster the data
-    # dbscan = DBSCAN(eps=0.1, min_samples=6) # 0.2,5
+    # dbscan = DBSCAN(eps=0.1, min_samples=6)
     data = np.column_stack((X_Vector, Y_Vector))
     # Scale the data
     scaler = RobustScaler()
     scaled_data = scaler.fit_transform(data)
-    dbscan = DBSCAN(eps=0.1, min_samples=4)  # 0.2,5
+    dbscan = DBSCAN(eps=0.2, min_samples=3)
     clusters = dbscan.fit_predict(scaled_data)
-    """
+
     # Plot the clusters
     plt.scatter(X_Vector, Y_Vector, c=clusters, cmap='viridis')
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.show()
-    """
+
     # Least Square Regression
     num_clusters = len(set(clusters)) - (1 if -1 in clusters else 0)  # account for noise points labeled as -1
     ARC, X_ARC, Y_ARC, RESIDUALS = [], [], [], []
@@ -149,7 +166,7 @@ def data_processing():
         x_arc = arc_points[:, 0]  # .flatten().tolist() #array
         y_arc = arc_points[:, 1]
 
-        if 8 < len(arc_points) < 80:
+        if 5 < len(arc_points) < 80:
             # check for the distance for each cluster
             first_x = x_arc[0]
             last_x = x_arc[-1]
@@ -157,26 +174,25 @@ def data_processing():
             last_y = y_arc[-1]
             distance_cluster = np.sqrt((first_x - last_x) ** 2 + (first_y - last_y) ** 2)
             print(distance_cluster)
-            if 90 < distance_cluster < 190:
+            if 60 < distance_cluster < 220:
                 X_ARC.append(x_arc)
                 Y_ARC.append(y_arc)
 
-    #for i in range(len(X_ARC)):
-        #plot(X_ARC[i], Y_ARC[i])
+    for i in range(len(X_ARC)):
+        plot(X_ARC[i], Y_ARC[i])
 
     # Convert Cartesian to polar
     D_ARC_1, A_ARC_1 = car2pol(X_ARC[0], Y_ARC[0])  # All same size
     D_ARC_2, A_ARC_2 = car2pol(X_ARC[1], Y_ARC[1])
     D_ARC_3, A_ARC_3 = car2pol(X_ARC[2], Y_ARC[2])
 
-    # Find the shortest distance and its angle
+    # Find the shortest distance for each arc and its angle
     # ARC1
     R1, Theta1 = getting_shortest_distance(D_ARC_1, A_ARC_1)
     # ARC2
     R2, Theta2 = getting_shortest_distance(D_ARC_2, A_ARC_2)
     # ARC3
     R3, Theta3 = getting_shortest_distance(D_ARC_3, A_ARC_3)
-
     if Theta1 < 0:
         Theta1 = Theta1 + (2 * math.pi)
     if Theta2 < 0:
@@ -193,9 +209,9 @@ def data_processing():
     theta1_degree = Theta1 * (180 / math.pi)
     theta2_degree = Theta2 * (180 / math.pi)
     theta3_degree = Theta3 * (180 / math.pi)
-    #print("R1: ", R1, "Theta 1 :", theta1_degree)
-    #print("R2: ", R2, "Theta 2 :", theta2_degree)
-    #print("R3: ", R3, "Theta 3 :", theta3_degree)
+    print("R1: ", R1, "Theta 1 :", theta1_degree)
+    print("R2: ", R2, "Theta 2 :", theta2_degree)
+    print("R3: ", R3, "Theta 3 :", theta3_degree)
     return R1,R2,R3,theta1_degree,theta2_degree,theta3_degree
 
 if __name__ == "__main__":
